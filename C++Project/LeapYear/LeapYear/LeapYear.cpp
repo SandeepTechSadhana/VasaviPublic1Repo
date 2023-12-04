4a0fc2acf3370a392ece80560b46db1f96891533
sandeep juluri <sandeep.juluri@kofax.com>
2023-12-04 19:51:17 +0530

    MyFirst Project for Leap Year

---
+++ C++Project/LeapYear/LeapYear/LeapYear.cpp
@@ -0,0 +1,15 @@
+// LeapYear.cpp
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int year;
+    cout << "Enter a year: ";
+    cin >> year;
+
+    return 0;
+}
+
