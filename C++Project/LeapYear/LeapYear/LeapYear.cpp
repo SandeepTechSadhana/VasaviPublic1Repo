e46acf840571de8b9ca69d0ed86d5b95170c3a05
saipavanvaddi <saipavan2930@gmail.com>
2023-12-15 20:15:02 +0530

    4c2 leapyear fix

--- C++Project/LeapYear/LeapYear/LeapYear.cpp
+++ C++Project/LeapYear/LeapYear/LeapYear.cpp
@@ -9,7 +9,18 @@ int main() {
     int year;
     cout << "Enter a year: ";
     cin >> year;
-if (year % 4 == 0) {
+    // fixed leap year logic
+if(year %100==0){
+    if(year%400!=0){
+        cout << year << " is a leap year";
+    }
+    else 
+    {
+        cout << year << " is not a leap year";
+    }
+    
+}
+else  {
 
         cout << year << " is a leap year.";
 
